2e81d7a1aaab32a27746cf3bf5a6336c8c301e13
Kenrick <kenrick37@gmail.com>
2017-03-05 16:19:37 +0300

    updated sidenav js

--- js/jquery-stick-sidenav.js
+++ js/jquery-stick-sidenav.js
@@ -1,6 +1,6 @@
 ;(function($, win, doc, undefined){
 
-  $.fn.kjsticky = function( options ) {
+  var kjsticky = $.fn.kjsticky = function( options ) {
 
       var $win = $(win),
           $base = $(this),
@@ -20,10 +20,6 @@
       sticky.sidebarBottom = sticky.sidebarTop + sticky.sidebarHeight;
       sticky.bottomStop = sticky.footerTop - sticky.sidebarHeight - (settings.offsetTop * 2);
 
-      console.log(sticky.bottomStop);
-      console.info($doc.height());
-      console.info(sticky.footerTop);
-
       function init () {
 
         if (settings.notification) {
@@ -40,32 +36,39 @@
       }
 
       var setSticky = {
+
+        // position nav element fixed to top of window with optional offset or IAW settings
         fixed: function( offset ) {
           clearPosition();
           $base.css({position: "fixed", top: offset || settings.offsetTop});
         },
-        absolute: function( offset ) {
+
+        // position the nav element absolute to top or bottom of parent container with optional offset or IAW settings
+        absolute: function( offset, where ) {
           clearPosition();
-          $base.css({position: "absolute", top: offset || settings.offsetTop});
+
+          if (where = "top") {
+            $base.css({position: "absolute", top: offset || settings.offsetTop});
+          } else {
+            $base.css({position: "absolute", bottom: offset || settings.offsetBottom});
+          }
         },
-        toBottom: function( offset ) {
-          clearPosition();
-          $base.css({position: "absolute", bottom: offset || settings.offsetBottom, marginBottom: 0});
-        }
       };
 
+      // clears the CSS properties and values so it won't interfere with what we are seeting.
       function clearPosition () {
         $base.css({position: "", top: "", bottom: ""});
       }
 
+      // compare the scrollTop position to breakpoints and set the position of the nav element fixed or absolute to top or bottom
       var checkScroll = function() {
 
         if ($win.scrollTop() > sticky.mainTop - settings.offsetTop && $win.scrollTop() <= sticky.bottomStop) {
-          setSticky.fixed();
+          setSticky.fixed(settings.offsetFixedTop);            // set the nav bar to be fixed to top of window after page is scrolled to top of nav element
         } else if ($win.scrollTop() > sticky.bottomStop) {
-          setSticky.toBottom("0");
+          setSticky.absolute(settings.offsetBottom, "bottom");           // position nav bar to bottom of parent container with settings.offsetBottom
         } else if ($win.scrollTop() < sticky.mainTop - settings.offsetTop) {
-          setSticky.absolute("0");
+          setSticky.absolute(settings.offsetTop, "top");              // position the nav bar to the top of the container with settings.offsetTop
         }
       };
 
@@ -79,8 +82,9 @@
     msg: "It's about to get sticky!",
     footerID: "footer",
     mainID: "wrapper-main",
-    offsetTop: 32,
-    offsetBottom: 16,
+    offsetTop: "0",  // I need to set a function in init() to stringify offsetTop/Bottom in case I enter a number instead.
+    offsetBottom: "0",
+    offsetFixedTop: 32,
     notification: false
   };
 
